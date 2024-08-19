80efbc7f67ba743d086ba1641217dfc3ce49b562
danielcaro26 <d.caro@uniandes.edu.co>
2024-08-18 23:53:44 -0500

    Agregar archivo py

---
+++ Google.py
@@ -0,0 +1,36 @@
+
+# Funciones numéricas adicionales
+import numpy as np
+
+# Lectura de datos y manejo de Data-sets
+import pandas as pd
+
+# Datos
+import yfinance as yf
+
+# Gráficos
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import statsmodels.api as sm
+import seaborn as sns
+
+#analisis tecnico
+import mplfinance as mpf
+
+# Probabilidad y estadística
+import math
+from scipy.stats import norm, chi2, jarque_bera,shapiro
+from scipy.optimize import brentq
+from scipy import stats
+
+# ARCH model
+from arch import arch_model
+
+# Engel test for ARCH effects
+from statsmodels.stats.diagnostic import het_arch
+
+# Descargamos datos historicos de la acción de Google
+df = yf.download('GOOGL', start='2005-01-01')
+
+#Hacer un gráfico de velas japonesas 
+mpf.plot(df,type='candle', volume=True,figratio=(19,8),style='yahoo',title='Google')
