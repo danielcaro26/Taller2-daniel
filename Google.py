a7961edeb75547c6422715ff51de6e7a703a4d87
NicolayB <ne.barrera@uniandes.edu.co>
2024-08-20 09:45:19 -0500

    Comparativa de inversion Google y S&P500

--- Google.py
+++ Google.py
@@ -23,14 +23,11 @@ from scipy.stats import norm, chi2, jarque_bera,shapiro
 from scipy.optimize import brentq
 from scipy import stats
 
-# ARCH model
-from arch import arch_model
-
 # Engel test for ARCH effects
 from statsmodels.stats.diagnostic import het_arch
 
 # Descargamos datos historicos de la acción de Google
-df = yf.download('GOOGL', start='2005-01-01')
+df = yf.download('GOOGL', start='2019-01-01')
 
 #Hacer un gráfico de velas japonesas 
 mpf.plot(df,type='candle', volume=True,figratio=(19,8),style='yahoo',title='Google')
@@ -75,37 +72,17 @@ plt.xlabel("Date")
 plt.ylabel("Volatility")
 plt.show()
 
-def black_scholes_call(S, K, r, sigma, T):
-    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
-    d2 = d1 - sigma*np.sqrt(T)
-    N = norm.cdf
-    return S*N(d1) - K*np.exp(-r*T)*N(d2)
+# Portafolio S&P 500
+df_sp500 = yf.download("^GSPC", start="2019-01-01")
 
-def implied_volatility(S, K, r, T, market_price, sigma_guess=0.30):
-    """
-    Calculate the implied volatility of a European call option using the Black-Scholes formula.
+# Creación de columna de una inversión de 100 USD
+df["Adj 100"] = (df["Adj Close"]*100)/df["Adj Close"][0]
+df_sp500["Adj 100"] = (df_sp500["Adj Close"]*100)/df_sp500["Adj Close"][0]
 
-    Parameters:
-    S (float): underlying asset price
-    K (float): option strike price
-    r (float): risk-free interest rate
-    T (float): time to maturity in years
-    market_price (float): observed market price of the option
-    sigma_guess (float, optional): initial guess for the volatility
-
-    Returns:
-    float: the implied volatility
-    """
-    def f(sigma):
-        return black_scholes_call(S, K, r, sigma, T) - market_price
-    return brentq(f, 0.0001, 1, xtol=1e-10, rtol=1e-10, maxiter=100000)
-
-
-S = 120
-K = 200
-r = 0.0499749
-T = 12
-market_price = 150
-
-implied_vol = implied_volatility(S, K, r, T, market_price)
-print("Implied volatility: {:.4f}".format(implied_vol))
+# Gráfica comparativa de invertir 100 USD en Google y en S&P 500 en el 2019
+plt.figure(figsize=(15,8))
+plt.plot(df["Adj 100"], color="blue")
+plt.plot(df_sp500["Adj 100"], color="red")
+plt.title("Google vs S&P 500")
+plt.legend(["Google","S&P 500"])
+plt.show()
